069f90e1ad051a43191f6270f84a0abfe7975019
DimitarStojmenovski <44978331+DimitarStojmenovski@users.noreply.github.com>
2018-11-12 18:20:28 +0100

    Add files via upload

---
+++ energija/helloKitty.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+
+
+  // disqus
+
+  /**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+var disqus_config = function () {
+this.page.url = file: D:/Rabota/Programiranje/FrontEnd/Brainster/Challenges/Treta%20samostojna/energija/helloKitty.html;  
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://tret-proekt.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+
+  // disqus
+  
